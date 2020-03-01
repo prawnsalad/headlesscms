afb2d09ade9d3e75a366ddd9efd39260cd6976d6
Darren <darren@darrenwhitlen.com>
2020-03-01 22:34:37 +0000

    Config file

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const Koa = require('koa');
 const Config = require('./libs/config');
 
-const config = global.config = new Config();
-const app = new Koa();
+async function main() {
+    const config = global.config = new Config();
+    await config.applyConfigFile(process.env.CONFIG || './config.yml');
 
-app.use(require('./middleware/apitoken'));
-app.use(require('./middleware/contentpath'));
+    const app = new Koa();
 
-let routes = require('./routes/main');
-app.use(routes.routes());
-app.use(routes.allowedMethods());
+    if (config.get('server.trustProxy')) {
+        app.proxy = true;
+    }
 
-app.listen(config.get('server.port', 3000));
+    app.use(require('./middleware/apitoken'));
+    app.use(require('./middleware/contentpath'));
+
+    let routes = require('./routes/main');
+    app.use(routes.routes());
+    app.use(routes.allowedMethods());
+
+    app.listen(config.get('server.port', 3000));
+}
+
+main();
--- libs/config.js
+++ libs/config.js
@@ -1,10 +1,13 @@
 const _ = require('lodash');
+const fs = require('fs').promises;
+const yaml = require('js-yaml');
 
 module.exports = class Config {
     constructor() {
         this.data = Object.assign(Object.create(null), {
             server: {
                 port: 3000,
+                trustProxy: false,
             },
             content: {
                 // %h = the website hostname
@@ -15,6 +18,24 @@ module.exports = class Config {
                 cache: false,
             },
         });
+    }
+
+    applyConfig(obj) {
+        this.data = _.merge(this.data, obj);
+    }
+
+    async applyConfigFile(filename) {
+        try {
+            let rawConfig = await fs.readFile(filename, {encoding: 'utf8'});
+            let confObj = yaml.safeLoad(rawConfig);
+            this.applyConfig(confObj);
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                console.error('Config file does not exist:', filename);
+            } else {
+                console.error('Error reading config file:', err.message);
+            }
+        }
     }
 
     get(key, def) {
