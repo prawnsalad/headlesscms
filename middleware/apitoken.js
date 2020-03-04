18d34178a1533d2882e24f8330e462f7316ebcfd
Darren <darren@darrenwhitlen.com>
2020-03-04 11:20:55 +0000

    Reading API token header correctly

--- middleware/apitoken.js
+++ middleware/apitoken.js
@@ -3,7 +3,7 @@ module.exports = async (ctx, next) => {
     let apiToken = ctx.query.apitoken || '';
 
     // Authorization: Bearer <token>
-    let auth = (ctx.headers['Authorization'] || '').split(' ');
+    let auth = (ctx.get('Authorization') || '').split(' ');
     if (auth && auth[0] === 'Bearer' && auth[1]) {
         apiToken = auth[1];
     }
